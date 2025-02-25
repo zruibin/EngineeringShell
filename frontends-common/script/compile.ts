2aa17cca9f3f3b8e99f83ea17cd24d2ecc13eb9c
ruibin.chow <ruibin.chow@qq.com>
2025-02-25 12:46:07 +0800

    [Frontend] Update compile.ts

--- frontends-common/script/compile.ts
+++ frontends-common/script/compile.ts
@@ -8,23 +8,82 @@ import path  from 'path';
 import fs from 'fs';
 import cryption from './cryption';
 
+// async function importFsExtra() {
+//   [
+//     process.cwd()
+//   ].forEach(p => {
+//     module.paths.push(p);
+//     module.paths.push(path.join(p, 'node_modules'));
+//   });
+//   const fsExtra = await import(`${require.resolve('fs-extra')}`);
+//   return fsExtra;
+// }
+
 const isUsingCryption = true;
+const distDir = './dist';
 
-const distPath = 'dist/renderer/';
-const fileName = 'index.js';
-const encryptFileName = 'index.ejs';
-const inputPath = path.join(distPath, fileName);
-const outputPath = path.join(distPath, encryptFileName);
+let excludeFiles: string[] = [];
 
-function encryptFile() {
+function encryptFile(dir: string, productName: string) {
+  const indexName = 'index.html';
+  const fileName = 'index.js';
+  const encryptFileName = 'index.ejs';
+  const inputFile = path.join(dir, fileName);
+  const outputFile = path.join(dir, encryptFileName);
+  const indexFile = path.join(dir, indexName);
+
   try {
-    const data = fs.readFileSync(inputPath, 'utf8');
+    const data = fs.readFileSync(inputFile, 'utf8');
     const encryptData = cryption.encrypt(data);
-    fs.writeFileSync(outputPath, encryptData);
-    console.error("encryptFile:", outputPath);
+    fs.writeFileSync(outputFile, encryptData);
+    console.log("encryptFile:", outputFile);
+    excludeFiles.push(`!${inputFile}`);
   } catch (error) {
     console.error("encryptFile error:", error);
   }
+
+  const originCode = `<script defer="defer" src="./${fileName}" />`;
+  const encryptCode = `
+    const encryptCode = window.bridge.sendSync("readAppFileSync", "${outputFile}");
+    const code = window.bridge.sendSync("cryption.decrypt", encryptCode);
+    eval(code);
+    `;
+
+    const indexContent = `
+  <!doctype html>
+  <html>
+  <head>
+      <meta charset="utf-8">
+      <title>${productName}</title>
+      <meta name="viewport" content="width=device-width,initial-scale=1">
+      ${ isUsingCryption ? '' : originCode }
+  </head>
+  <body>
+  <script>
+    ${ isUsingCryption ? encryptCode : '' }
+  </script>
+  </body></html>
+  `;
+
+  fs.writeFile(indexFile, indexContent, (error) => {
+    if (error) {
+      console.error(`修改index.html失败, error: ${error}`);
+    } else {
+      console.log("修改index.html成功！");
+    }
+  });
+}
+
+function isArrayIncluded(arr1, arr2) {
+  return arr2.every(item => arr1.includes(item));
+}
+
+function getDirectoriesSync(dirPath) {
+  return fs.readdirSync(dirPath)
+    .filter(item => {
+      const fullPath = path.join(dirPath, item);
+      return fs.statSync(fullPath).isDirectory() && item !== 'main';
+    });
 }
 
 async function complieFunction() {
@@ -41,7 +100,7 @@ async function complieFunction() {
   }
 
   // 创建dist/main/boot.js
-  const mainEntry = "dist/main/boot.js";
+  const mainEntry = path.join(distDir, 'main/boot.js');
   const bootPath = path.join(projectDir, mainEntry);
   const bootContent = `
 require('bytenode');
@@ -56,60 +115,27 @@ require('./index.jsc');
   });
 
   if (isUsingCryption) {
-    encryptFile();
+    const encryptDirs = getDirectoriesSync(distDir);
+    console.log(`encryptDirs: ${JSON.stringify(encryptDirs)}`);
+    encryptDirs.forEach(value => {
+      encryptFile(path.join(distDir, value), packageJson?.productName ?? '');
+    });
   }
 
-  // 修改dist/renderer/index.html
-  const indexEntry = "dist/renderer/index.html";
-  const indexPath = path.join(projectDir, indexEntry);
-
-  const originCode = `<script defer="defer" src="./${fileName}" />`;
-  const encryptCode = `
-  const encryptCode = window.bridge.sendSync("readAppFileSync", "${outputPath}");
-  const code = window.bridge.sendSync("cryption.decrypt", encryptCode);
-  eval(code);
-  `;
-
-  const indexContent = `
-<!doctype html>
-<html>
-<head>
-    <meta charset="utf-8">
-    <title>${packageJson?.productName}</title>
-    <meta name="viewport" content="width=device-width,initial-scale=1">
-    ${ isUsingCryption ? '' : originCode }
-</head>
-<body>
-<script>
-  ${ isUsingCryption ? encryptCode : '' }
-</script>
-</body></html>
-`;
-
-  fs.writeFile(indexPath, indexContent, (error) => {
-    if (error) {
-      console.log(`修改index.html失败, error: ${error}`);
-    } else {
-      console.log("修改index.html成功！");
-    }
-  });
-
   // 修改package.json中main入口
-  const newMainEntry = "dist/main/boot.js";
   try {
     let wirte = false;
-    if (packageJson.main !== newMainEntry) {
+    if (packageJson?.main !== mainEntry) {
       wirte = true;
-      packageJson.main = newMainEntry;
-      console.log(`package.json已更新，main字段设置为: ${newMainEntry}`);
+      packageJson.main = mainEntry;
+      console.log(`package.json已更新，main字段设置为: ${mainEntry}`);
     }
 
-    const excludeFile = `!${inputPath}`;
-    if (isUsingCryption && packageJson.build?.files && 
-        !packageJson.build.files.includes(excludeFile)) {
+    if (isUsingCryption && packageJson?.build?.files && 
+        !isArrayIncluded(packageJson?.build.files, excludeFiles)) {
       wirte = true;
-      packageJson.build.files.push(path.join(excludeFile));
-      console.log(`package.json已更新，build.files字段设置为: ${JSON.stringify(packageJson.build.files)}`);
+      packageJson?.build.files.push(...excludeFiles);
+      console.log(`package.json已更新，build.files字段设置为: ${JSON.stringify(packageJson?.build.files)}`);
     }
     if (wirte) {
       const data = JSON.stringify(packageJson, null, 2);
